f606f206e4a5258303189618ad92cc1d81309385
Alexander Ortiz <CL198785156@enel.com>
2024-05-01 02:12:17 -0400

    add my-score page

---
+++ src/app/scoreboard/pages/my-score-page/my-score-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scoreboard-my-score',
+  templateUrl: './my-score-page.component.html',
+  styleUrls: ['./my-score-page.component.css'],
+})
+export class MyScorePageComponent {}
